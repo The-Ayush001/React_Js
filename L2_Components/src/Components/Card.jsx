66d6c5c4fbe8f7de4474d5013d59d5ab125b3033
The-Ayush001 <ayush007arm@.com>
2026-05-29 17:51:33 +0530

    Components in React Js

---
+++ L2_Components/src/Components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Hi i am Ayush</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, beatae?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
